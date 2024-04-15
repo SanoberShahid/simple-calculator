289c605a07f9f8b82f92762b6800e8ba590d4bb5
SanoberShahid <sheikhshahidsheikh794@gmail.com>
2024-04-15 11:02:46 -0700

    push my calculator to github

---
+++ main.ts
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+import inquirer from "inquirer"
+
+const MyCalulator = await inquirer.prompt([
+    {
+        type:'list',
+        name:'operation',
+        message:'what operation you want to perform?',
+        choices:['add','subtract','multiply','division']
+    },
+    {
+        
+        type:'number',
+        name:'num1',
+        message:'enter first number',
+    },
+    {
+        type:'number',
+        name:'num2',
+        message:'enter other number'
+    }
+])
+
+if(MyCalulator.operation === 'add'){
+   console.log(`the sum of your numbr is:${MyCalulator.num1 + MyCalulator.num2}`);
+}else if(MyCalulator.operation === 'subtract'){
+    console.log(`the subtract of your numbr is:${MyCalulator.num1 - MyCalulator.num2}`);
+}else if(MyCalulator.operation === 'multiply'){
+   console.log(`the multiply of your numbr is:${MyCalulator.num1 * MyCalulator.num2}`);
+}else {
+    console.log(`the division of your numbr is:${MyCalulator.num1 / MyCalulator.num2}`); 
+}
+
+
